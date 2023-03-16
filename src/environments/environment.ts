d79e35e51163aa37d1300d393ecdb473927659d2
ismailovic77 <ismail03042000@gmail.com>
2023-03-16 17:58:13 +0100

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'cryptoapp-c6f51',
+    appId: '1:771070844154:web:bbade31c8f149e17093d43',
+    storageBucket: 'cryptoapp-c6f51.appspot.com',
+    apiKey: 'AIzaSyC0l7gEoBQnGyP-X6wZ5jDpuU4NuPkH6Ps',
+    authDomain: 'cryptoapp-c6f51.firebaseapp.com',
+    messagingSenderId: '771070844154',
+  },
+    production : false
+}
