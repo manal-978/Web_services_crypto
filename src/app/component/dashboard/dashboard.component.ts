7893b1459d5acafb6f732b32cbba1fb7db2071d3
manal-978 <herradimanal3105@gmail.com>
2023-03-20 11:43:15 +0100

    ondaypush

---
+++ src/app/component/dashboard/dashboard.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+  constructor(private auth : AuthService) {}
+
+  ngOnInit(): void {
+
+  }
+
+  register() {
+    this.auth.logout();
+  }
+
+
+}
